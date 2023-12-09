7c0b37ff5d93eb077b984ad8fc88b41544600198
Dan Guan <daniel.guan.bca@gmail.com>
2023-12-09 14:11:46 -0500

    Add day 9 input and solutions

---
+++ day9/solution2.py
@@ -0,0 +1,53 @@
+#! /usr/bin/python3.10
+import csv
+from typing import List
+
+
+class Solution(object):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def extrapolate_value(self, seq: List[int]) -> int:
+        """Extrapolates the prev value in seq based on the sequence diffs.
+
+        When finding the previous value in each intermediate diff sequence,
+        append the "previous value" at end of the diff list.
+        """
+        diffs = [seq]
+
+        while not (diffs[-1][0] == 0 and diffs[-1][-1] == 0):
+            curr_diffs = []
+
+            for idx in range(1, len(diffs[-1])):
+                curr_diffs.append(diffs[-1][idx] - diffs[-1][idx - 1])
+
+            diffs.append(curr_diffs)
+
+        # 0 is the "previous value" of diffs[-1]
+        diffs[-1].append(0)
+
+        for diff_idx in range(len(diffs) - 2, -1, -1):
+            # Use the next diff's latest value to find the new prev value.
+            diffs[diff_idx].append(diffs[diff_idx][0] - diffs[diff_idx + 1][-1])
+
+        return diffs[0][-1]
+
+    def solve(self):
+        seqs = []
+        extrapolated_value_sum = 0
+
+        with open(self.filename) as f:
+            csv_reader = csv.reader(f)
+
+            for row in csv_reader:
+                seqs.append([int(num) for num in row[0].split(" ")])
+
+        for seq in seqs:
+            extrapolated_value_sum += self.extrapolate_value(seq)
+
+        print(extrapolated_value_sum)
+
+
+if __name__ == "__main__":
+    sol = Solution("input.csv")
+    sol.solve()
