d01418ed0de05fe4b813408d1af2878b951abd00
Dan Guan <daniel.guan.bca@gmail.com>
2023-12-13 00:54:10 -0500

    Add day 12 input and solutions

---
+++ day12/solution1.py
@@ -0,0 +1,79 @@
+#! /usr/bin/python3.10
+import csv
+import functools
+from typing import List
+
+
+class Solution(object):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def get_record_arrangements(self, record: str, criteria: List[int]) -> int:
+        """Gets the total number of valid arrangements for the given record."""
+        latest_broken = None
+        for c_idx in range(len(record) - 1, -1, -1):
+            if record[c_idx] == "#":
+                latest_broken = c_idx
+                break
+
+        @functools.cache
+        def recurse(start_idx: int, criteria_idx: int) -> int:
+            # All springs in criteria have been placed
+            if criteria_idx == len(criteria):
+                # All guaranteed broken springs are accounted for
+                if latest_broken is None or start_idx > latest_broken:
+                    return 1
+                # There is an unfulfilled broken spring
+                else:
+                    return 0
+            # Some criteria have not been placed
+            elif start_idx >= len(record):
+                return 0
+
+            valid_arrangements = 0
+            crit_size = criteria[criteria_idx]
+
+            for idx in range(start_idx, len(record) - crit_size + 1):
+                # A broken spring was skipped
+                if idx > 0 and record[idx - 1] == "#":
+                    break
+
+                is_valid = True
+                for curr_crit_idx in range(idx, idx + crit_size):
+                    # Attempting to place broken spring on unbroken space
+                    if record[curr_crit_idx] == ".":
+                        is_valid = False
+                        break
+
+                # Check if there is an appropriate gap between criteria
+                if (
+                    (idx + crit_size == len(record) and criteria_idx < len(criteria) - 1)
+                    or (idx + crit_size < len(record) and record[idx + crit_size] == "#")
+                ):
+                    is_valid = False
+
+                if is_valid:
+                    valid_arrangements += recurse(idx + crit_size + 1, criteria_idx + 1)
+
+            return valid_arrangements
+
+        return recurse(0, 0)
+
+    def solve(self):
+        total_arrangements = 0
+
+        with open(self.filename) as f:
+            csv_reader = csv.reader(f)
+
+            for row in csv_reader:
+                record, crit_0 = row[0].split(" ")
+                criteria = [int(crit_0), *[int(num) for num in row[1:]]]
+
+                total_arrangements += self.get_record_arrangements(record, criteria)
+
+        print(total_arrangements)
+
+
+if __name__ == "__main__":
+    sol = Solution("input.csv")
+    sol.solve()
